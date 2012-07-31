a65b3a691577fb6e5c6c774925c352337f1fb04f
Ivo van der Wijk <github@in.m3r.nl>
2012-07-31 11:16:24 +0200

    make paste optional

--- twotest/management/commands/ptest.py
+++ twotest/management/commands/ptest.py
@@ -15,12 +15,17 @@ class Command(BaseCommand):
             dest='keyword',
             default='',
             help='Limit by keyword'),
+        optparse.make_option('-p',
+            action='store_false',
+            dest='paste',
+            default=True,
+            help='Paste result to pastebin'),
         )
 
     def handle(self, *a, **b):
         ## http://pytest.org/latest/usage.html
         ## --tb=native is also usable
-        opts = ["--tb=short", "--pastebin=all"]
+        opts = ["--tb=short"]
 
         settings.DATABASES = dict(default=settings.TEST_DB)
         if not b.get('capture'):
@@ -28,6 +33,8 @@ class Command(BaseCommand):
         if b.get('keyword') and b['keyword']:
             opts.append("-k")
             opts.append(b['keyword'])
+        if not b.get('paste'):
+            opts.append("--pastebin=all")
 
         pytest.main(opts + list(a))
 
